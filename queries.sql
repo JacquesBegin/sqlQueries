4cabb8350c96f72158e6531a4bfe039a2f9884a8
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-03 20:39:31 -0500

    Adds distinct query

--- queries.sql
+++ queries.sql
@@ -4,3 +4,5 @@ SELECT column_names FROM table_name;
 /* Select all data from a table */
 SELECT * FROM table_name;
 
+/* Select only different/distinct data from table */
+SELECT DISTINCT column_names FROM table_name;
