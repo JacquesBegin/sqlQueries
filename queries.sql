cb2e40e2e22eadf43487ca56030d353bf8712fe4
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-03 22:18:19 -0500

    Adds count to select query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+/* Used https://www.w3schools.com/sql as a reference for these queries */
+
 /* A query to select data from a table */
 SELECT column_names FROM table_name;
 
@@ -6,3 +8,8 @@ SELECT * FROM table_name;
 
 /* Select only different/distinct data from table */
 SELECT DISTINCT column_names FROM table_name;
+
+/* List the number of distinct entries in return-set */
+SELECT COUNT(DISTINCT column_name) FROM table_name;
+/* or */
+/* SELECT COUNT(*) AS temp_name FROM (SELECT DISTINCT column_name FROM table_name); */
